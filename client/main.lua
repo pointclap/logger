3fca6b7fa77c2ed9805d5ed0bd5894bd18c5d3a1
Mathias Pius <contact@pius.io>
2023-05-27 14:56:44 +0200

    Merge main

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,7 @@
 require("client.network")
 require("client.messages")
 require("client.systems.players")
+require("client.systems.physics")
 
 players = {}
 username = nil
@@ -16,63 +17,12 @@ love.graphics.setFont(font)
 function love.load(args)
 	username = args[2]
 	connection = connect(args[1]);
+
+	init_physics()
 end
 
 function love.update(dt)
 	cur_time = cur_time + dt
-	-- Update current player location to mouse position
-	if localplayer and cur_time > next_update then
-		next_update = cur_time + (1.0 / 128)
-
-		--local x, y = love.mouse.getPosition()
-		local x = players[localplayer].x
-		local y = players[localplayer].y
- 
-        local mouseX = players[localplayer].mouseX
-        local mouseY = players[localplayer].mouseY
-
-		if love.window.hasMouseFocus() then
-			local mouseX, mouseY = love.mouse.getPosition()
-			local ms = 1000.0 * dt
-
-			if love.keyboard.isDown("d") then
-				x = x + ms
-			elseif love.keyboard.isDown("a") then
-				x = x - ms
-			end
- 
-			if love.keyboard.isDown("s") then
-				y = y + ms
-			elseif love.keyboard.isDown("w") then
-				y = y - ms
-			end
-		end
- 
-		if players[localplayer].x ~= x or players[localplayer].y ~= y then
-			connection:send({
-				cmd = "update-position",
-				id = localplayer,
-				x = x,
-				y = y,
-			})
- 
-			players[localplayer].x = x
-			players[localplayer].y = y
-		end
-		
-        if players[localplayer].mouseX ~= mouseX or players[localplayer].mouseY ~= mouseY then
-            connection:send({
-                cmd = "update-mouse",
-                id = localplayer,
-                mouseX = mouseX,
-                mouseY = mouseY
-            })
-
-            players[localplayer].mouseX = mouseX
-            players[localplayer].mouseY = mouseY
-        end
-	end
- 
 	if connection then
 		for _, event in pairs(connection:events()) do
 			if event.type == "receive" then
@@ -90,6 +40,12 @@ function love.update(dt)
 			end
 		end
 	end
+
+	player_movement(dt)
+	apply_drag(dt)
+	update_physics(dt)
+	interpolate_player_location(dt)
+	send_updated_position()
 end
 
 function love.quit()
@@ -102,48 +58,11 @@ function love.quit()
 end
 
 function love.draw()
-	local color_index = 1
 	local width, height = love.graphics.getDimensions()
 
 	if localplayer then
-		love.graphics.translate(-players[localplayer].x + width / 2, -players[localplayer].y + height / 2)
+		love.graphics.translate(-players[localplayer].model.x + width / 2, -players[localplayer].model.y + height / 2)
 	end
  
-	for id, player in pairs(players) do
-		local colour = hsl2rgb((id-1)/12)
-		
-		love.graphics.setColor(colour.r, colour.g, colour.b, colour.a)
-
-		love.graphics.circle("fill", player.x, player.y, 10)
-
-		-- if player.username then
-		-- 	local text = love.graphics.newText(font, {colour, player.username})
-		-- 	love.graphics.scale(2)
-		-- 	local textWidth, textHeight = text:getDimensions()
-		-- 	love.graphics.draw(text, player.x - textWidth / 2, player.y - 10 - textHeight / 2)
-		-- 	love.graphics.scale(0.5)
-		-- end
-
-		color_index = color_index + 1
-	end
-end
-
--- Converts HSL to RGB. (input and output range: 0 - 1)
-function hsl2rgb(h, s, l, a)
-	if s == nil then s = 1 end
-	if l == nil then l = 0.5 end
-	if a == nil then a = 1 end
-
-	if s<=0 then return {r=l,g=l,b=l,a=a} end
-	h, s, v = h*6, s, l
-	local c = (1-math.abs(2*l-1))*s
-	local x = (1-math.abs(h%2-1))*c
-	local m,r,g,b = (l-.5*c), 0,0,0
-	if h < 1     then r,g,b = c,x,0
-	elseif h < 2 then r,g,b = x,c,0
-	elseif h < 3 then r,g,b = 0,c,x
-	elseif h < 4 then r,g,b = 0,x,c
-	elseif h < 5 then r,g,b = x,0,c
-	else              r,g,b = c,0,x
-	end return {r=r+m, g=g+m, b=b+m, a=a}
+	render_player_model()
 end
---
+++ client/systems/physics.lua
@@ -0,0 +1,76 @@
+local world = nil
+local velocity_iterations = 8
+local position_iterations = 3
+local drag_coefficient = 5
+
+function init_physics()
+    love.physics.setMeter(64)
+    world = love.physics.newWorld(0, 0, true)
+end
+
+function update_physics(dt)
+    for _, player in pairs(players) do
+        if player.body ~= nil then
+            if player.velocity ~= nil then
+                player.body:setLinearVelocity(player.velocity.x, player.velocity.y)
+            end
+
+            if player.position ~= nil then
+                player.body:setPosition(player.position.x, player.position.y)
+            end
+        end
+    end
+
+    world:update(dt, velocity_iterations, position_iterations)
+
+    for _, player in pairs(players) do
+        if player.body then
+            if player.velocity then
+                local x, y = player.body:getLinearVelocity()
+                player.velocity = {
+                    x = x,
+                    y = y
+                }
+            end
+
+            if player.position ~= nil then
+                local x, y = player.body:getPosition()
+                player.position = {
+                    x = x,
+                    y = y
+                }
+            end
+        end
+    end
+end
+
+function apply_drag(dt)
+    for _, player in pairs(players) do
+        if player.body then
+            local x, y = player.body:getLinearVelocity()
+            player.body:applyForce(-x * drag_coefficient, -y * drag_coefficient)
+        end
+    end
+end
+
+subscribe_message("new-player", function(msg)
+    if players[tonumber(msg.id)] == nil then
+        players[tonumber(msg.id)] = {}
+    end
+
+    players[tonumber(msg.id)].velocity = {
+        x = 0,
+        y = 0
+    };
+
+    players[tonumber(msg.id)].position = {
+        x = 0,
+        y = 0
+    }
+
+    local body = love.physics.newBody(world, 0, 0, "dynamic");
+    local shape = love.physics.newCircleShape(20)
+    love.physics.newFixture(body, shape, 5)
+
+    players[tonumber(msg.id)].body = body
+end)
