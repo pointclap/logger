5c712ae08bc2900cd8389130c71bc6c376668a43
Mathias Pius <contact@pius.io>
2023-05-30 21:17:59 +0200

    Space out the pings a lil

--- systems/server/ping.lua
+++ systems/server/ping.lua
@@ -1,14 +1,16 @@
 hooks.add("update", function(dt)
+    local current_time = love.timer.getTime()
+
     for _, player in entities.players() do
         if not player.latency then
             player.latency = 0.0
         end
 
-        if not player.ping and player.peer then
+        if (not player.ping or current_time - player.ping > 5.0) and player.peer then
             player.peer:send({
                 cmd = "ping"
             })
-            player.ping = love.timer.getTime()
+            player.ping = current_time
         end
     end
 end)
@@ -17,7 +19,6 @@ messages.subscribe("pong", function(peer, msg)
     for _, player in entities.players() do
         if player.ping and player.peer:index() == peer:index() then
             player.latency = love.timer.getTime() - player.ping
-            player.ping = nil
         end
     end
 end)
